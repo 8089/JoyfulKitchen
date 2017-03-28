8f0e0556d88bc48776f2f3d961b223a6b560269c
8089 <342152785@qq.com>
2017-03-28 19:43:38 +0800

    Rejected commit: Default

--- app/src/main/java/com/joyful/joyfulkitchen/activity/MainActivity.java
+++ app/src/main/java/com/joyful/joyfulkitchen/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.joyful.joyfulkitchen;
+package com.joyful.joyfulkitchen.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.joyful.joyfulkitchen.R;
 
 public class MainActivity extends AppCompatActivity {
 
